31721a17579198fb4094f3ba60f62c6866cdfdc7
Evgeniy Fateev <spaceflow@gmail.com>
2020-06-28 18:34:15 +0300

    feat(app): setup error handling

--- Gemfile
+++ Gemfile
@@ -9,8 +9,7 @@ gem 'rails', '~> 6.0.3'
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-# gem 'jbuilder', '~> 2.7'
+gem 'jsonapi-serializer', '~> 2.0.0'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
@@ -24,6 +23,9 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
+
+gem 'dry-initializer', '~> 3.0.3'
+gem 'dry-validation', '~> 1.5.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/controllers/concerns/api_errors.rb
@@ -0,0 +1,33 @@
+module ApiErrors
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from(StandardError) { |e| handle_exception(e) }
+  end
+
+  private
+
+  def handle_exception(e)
+    case e
+    when ActiveRecord::RecordNotFound
+      error_response(I18n.t(:not_found, scope: 'api.errors'), :not_found)
+    when ActiveRecord::RecordNotUnique
+      error_response(I18n.t(:not_unique, scope: 'api.errors'), :unprocessable_entity)
+    when Validations::InvalidParams, KeyError
+      error_response(I18n.t(:missing_parameters, scope: 'api.errors'), :unprocessable_entity)
+    else
+      raise
+    end
+  end
+
+  def error_response(error_messages, status)
+    errors = case error_messages
+    when ActiveRecord::Base
+      ErrorSerializer.from_model(error_messages)
+    else
+      ErrorSerializer.from_messages(error_messages)
+    end
+
+    render json: errors, status: status
+  end
+end
